9fb27bcf87b4b67e3cdb95147d0e6684bb42e2a2
Ethel Videa <100855553+EthelVidea20@users.noreply.github.com>
2023-08-27 15:43:13 -0600

    Inicio sesion

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+
+
+
+?>
+
+<!--CREACION DE PAGINA PRINCIPAL-->
+
+<head>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<header>
+    <div class="container_header">
+
+        <div class="logo">
+            <a href="./index.php">
+                <img src="./assets/img/Logo_G_P.png" alt="">
+            </a>
+        </div>
+
+        <div class="menu">
+            <nav id="nav">
+                <ul>
+                    <li>Funcionalidades</li>
+                    <li>Quienes somos</li>
+
+                    <li>Registrate</li>
+
+                </ul>
+
+
+            </nav>
+
+        </div>
+
+    </div>
+
+</header>
+
+<body>
+    <div class="portada">
+        <h2>GP es una herramienta que te ayudara a gestionar tus presupuestos!</h2>
+        <img src="./assets/img/portadagp.png" alt="">
+    </div>
+
+
+</body>
---
+++ iniciosesion.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="./assets/css/estyleses.css">
+</head>
+
+<body>
+    <main>
+        <form action="" method="post">
+
+            <div class="inicio_seccion">
+                <h1>Inicia sesion</h1>
+                <img src="./assets/img/usuario.png" alt="">
+                <div class="datos">
+                    <p>Correo Electronico</p>
+                    <input type="text">
+                    <p>Contraseña</p>
+                    <input type="text">
+                    <input type="submit" class="btn">
+
+                </div>
+            </div>
+
+        </form>
+
+
+    </main>
+</body>
+
+</html>
